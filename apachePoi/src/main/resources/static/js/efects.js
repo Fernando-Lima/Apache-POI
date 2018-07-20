b52b866e14035aadb183947d4f68556d0b63b7c6
Fernando-Lima <fernandosupweb@gmail.com>
2018-07-20 09:59:41 -0300

    Implementado classe controller e view index para importação do arquivo xls

---
+++ apachePoi/src/main/resources/static/js/efects.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    // show the alert
+    setTimeout(function() {
+        $(".alert").alert('close');
+    }, 5000);
+});
+
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
+	var button = $(event.relatedTarget);
+	var codigo = button.data('codigo');
+	var descricao = button.data('descricao');
+	var url = button.data('passaurl');
+	//alert(url).show;
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	var action = form.data('url-base');
+	if(!action.endsWith('/')){
+		action += '/';
+	}
+	form.attr('action', action + url + codigo);
+	modal.find('.modal-body span').html('Tem certeza de deseja excluir <strong>' + descricao + '</strong> ?');
+});
+
+
+(function($){
+	$(document).ready(function(){
+
+	$('#cssmenu li.active').addClass('open').children('ul').show();
+		$('#cssmenu li.has-sub>a').on('click', function(){
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			}
+			else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+
+	});
+})(jQuery);
